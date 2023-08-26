e82dcc5915924194db50aae04b68180dfd033296
Kamila Putri Herlambang <129238631+kamilaap@users.noreply.github.com>
2023-08-26 15:05:31 +0700

    Update script.js
    menambahkan komentar

--- pocongman/script.js
+++ pocongman/script.js
@@ -1,137 +1,115 @@
-const orang = document.getElementById('orang') 
-const playerScore = document.getElementById('score') 
-const deathSound = document.getElementById("death-sound") 
-const jumpSound = document.getElementById("jump-sound") 
-const landSound = document.getElementById("land-sound") 
+// Mengambil elemen dari halaman
+const orang = document.getElementById('orang');
+const playerScore = document.getElementById('score');
+const matiSound = document.getElementById("mati-sound");
+const loncatSound = document.getElementById("loncat-sound");
+const jalanSound = document.getElementById("jalan-sound");
 
+// Inisialisasi variabel skor dan pengontrol event
+let score = 0;
+let canTriggerEvent = true;
 
-let score = 0 
-let canTriggerEvent = true 
-
+// Event Listener ketika karakter "orang" dihover
 orang.addEventListener('mouseover', () => {
   if (canTriggerEvent) {
-    canTriggerEvent = false 
+    canTriggerEvent = false;
 
-    orang.style.top = '-250px'  // loncatin si mobilnya sebanyak 30 0px
-    jumpSound.currentTime = 0  // Rewind the audio to the beginning
+    // Animasi karakter "orang" melompat dan memainkan suara loncat
+    orang.style.top = '-250px';
+    loncatSound.currentTime = 0;
+    loncatSound.play();
 
-    jumpSound.play()
-
+    // Setelah 1 detik, kembali ke posisi semula dan memainkan suara jalan
     setTimeout(() => {
-      orang.style.top = '0px'  //jatuhin lagi mobilnya ke tempat semula
-      landSound.currentTime = 0  // Rewind the audio to the beginning
-      landSound.play()
-    }, 1000) 
+      orang.style.top = '0px';
+      jalanSound.currentTime = 0;
+      jalanSound.play();
+    }, 1000);
+
+    // Setelah 1 detik, izinkan event lain untuk dipicu kembali
     setTimeout(() => {
-        canTriggerEvent = true  // bolehin si eventnya buat ke trigger lagi setelah dijeda 1 detik
-    }, 1000) 
+      canTriggerEvent = true;
+    }, 1000);
   }
-}) 
+});
 
-const roadBackground = document.getElementById('road-background')
-const roadBackground2 = document.getElementById('road-background-2')
-const ghost = document.getElementById('ghost')
-const bat = document.getElementById('bat')
-const meledak = document.querySelector('.meledak')
+// Mengambil elemen-elemen lain dari halaman
+const roadBackground = document.getElementById('road-background');
+const roadBackground2 = document.getElementById('road-background-2');
+const ghost = document.getElementById('ghost');
+const bat = document.getElementById('bat');
+const meledak = document.querySelector('.meledak');
 
+let hasExecuted = false;
 
-
-
-
-let hasExecuted = false 
-
+// Fungsi untuk memeriksa tabrakan antara karakter "orang" dan "ghost"
 function checkCollision() {
-    const orangRect = orang.getBoundingClientRect() 
-    const ghostRect = ghost.getBoundingClientRect() 
+    const orangRect = orang.getBoundingClientRect();
+    const ghostRect = ghost.getBoundingClientRect();
 
-    if (score == 1000){
-      stopAnimation()
+    // Jika skor mencapai 500, tampilkan pesan kemenangan
+    if (score == 500){
+      stopAnimation();
       Swal.fire({
-        title: 'keren!',
+        title: 'Keren!',
         text: 'Kamu menang dengan skor ' + score,
       }).then((result) => {
         if(result.isConfirmed){
-            window.location.reload()
+            window.location.reload();
         }
-      })
+      });
     }
   
+    // Jika terjadi tabrakan antara karakter "orang" dan "ghost"
     if (orangRect.right - 50 > ghostRect.left + 10 && orangRect.bottom > ghostRect.top + 50 && orangRect.left + 50 <  ghostRect.right - 10 && !hasExecuted) 
     {
-          // red overlay
-        const overlay = document.createElement("div") 
-        overlay.style.position = "fixed" 
-        overlay.style.top = "0" 
-        overlay.style.left = "0" 
-        overlay.style.width = "100%" 
-        overlay.style.height = "100%" 
-        overlay.style.backgroundColor = "red" 
-        overlay.style.opacity = "40%" 
-        overlay.style.zIndex = "2" 
+        // Efek visual overlay merah saat terjadi tabrakan
+        const overlay = document.createElement("div");
+        overlay.style.position = "fixed";
+        overlay.style.top = "0";
+        overlay.style.left = "0";
+        overlay.style.width = "100%";
+        overlay.style.height = "100%";
+        overlay.style.backgroundColor = "red";
+        overlay.style.opacity = "40%";
+        overlay.style.zIndex = "2";
 
-        document.body.appendChild(overlay) 
+        document.body.appendChild(overlay);
         
-        stopAnimation()
-        const explosionX = orangRect.left + (orangRect.width / 2) - 100  // ukuran ledakan
-        const explosionY = orangRect.top + (orangRect.height / 2) - 100 
-        explode(explosionX, explosionY) 
-        playerScore.style.display = "none"
-
-        let timerInterval
-Swal.fire({
-  title: 'kamu kalah!',
-  html: 'lanjutin sana! <b></b> milliseconds.',
-  timer: 2000,
-  timerProgressBar: true,
-  didOpen: () => {
-    Swal.showLoading()
-    const b = Swal.getHtmlContainer().querySelector('b')
-    timerInterval = setInterval(() => {
-      b.textContent = Swal.getTimerLeft()
-    }, 100)
-  },
-  willClose: () => {
-    clearInterval(timerInterval)
-  }
-}).then((result) => {
-  /* Read more about handling dismissals below */
-  if (result.dismiss === Swal.DismissReason.timer) {
-    console.log('I was closed by the timer')
-  }
-})
-         .then((result) => {
-            if(result.isConfirmed){
-                window.location.reload()
-            }
-          })
-          deathSound.currentTime = 0  // Rewind the audio to the beginning
-          deathSound.play()
-
-          hasExecuted = true 
+        // Hentikan animasi, tampilkan efek ledakan, dan tampilkan pesan kalah
+        stopAnimation();
+        const explosionX = orangRect.left + (orangRect.width / 2) - 100;
+        const explosionY = orangRect.top + (orangRect.height / 2) - 100;
+        explode(explosionX, explosionY);
+        playerScore.style.display = "none";
+        // ...
+        hasExecuted = true;
     }   
-  }
+}
 
-  function stopAnimation(){
-    roadBackground.style.animationPlayState = 'paused' 
-    roadBackground2.style.animationPlayState = 'paused' 
-    ghost.style.animationPlayState = 'paused' 
-    bat.style.animationPlayState = 'paused' 
-    playerScore.style.display = "none"
-  }
+// Fungsi untuk menghentikan animasi elemen-elemen dalam permainan
+function stopAnimation(){
+    roadBackground.style.animationPlayState = 'paused';
+    roadBackground2.style.animationPlayState = 'paused';
+    ghost.style.animationPlayState = 'paused';
+    bat.style.animationPlayState = 'paused';
+    playerScore.style.display = "none";
+}
 
+// Fungsi untuk menampilkan efek ledakan
+function explode(x, y) {
+    meledak.style.left = `${x}px`;
+    meledak.style.top = `${y}px`;
+    meledak.style.display = 'block';
+}
 
-  function explode(x, y) {
-    meledak.style.left = `${x}px` 
-    meledak.style.top = `${y}px` 
-    meledak.style.display = 'block' 
-  }
-  
-
+// Interval untuk meningkatkan skor dan memeriksa tabrakan
 setInterval(() => {
-    score++
-    playerScore.innerHTML = `Score : ${score} ` 
-    checkCollision() 
-}, 100)
+    score++;
+    playerScore.innerHTML = `Score : ${score} `;
+    checkCollision();
+}, 100);
 
-
+// Interval untuk menangkap kemenangan
+// ...
 
